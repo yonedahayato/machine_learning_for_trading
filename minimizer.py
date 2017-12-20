29d6023f5b9ff2687cb8fc76185dcb6c49f76049
yonedahayato <yhayato1320@gmail.com>
2017-12-20 15:00:49 +0900

    add plot function

--- minimizer.py
+++ minimizer.py
@@ -14,5 +14,12 @@ def test_run():
     print("Minima found at:")
     print("x = {}, y = {}".format(min_result.x, min_result.fun))
 
+    Xplot = np.linspace(0.5, 2.5, 21)
+    Yplot = f(Xplot)
+    plt.plot(Xplot, Yplot)
+    plt.plot(min_result.x, min_result.fun, "ro")
+    plt.title("Minima of an objective function")
+    plt.show()
+
 if __name__ == "__main__":
     test_run()
