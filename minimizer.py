7a64ef34cb881121f86847c8ed48214190739288
yoneda hayato <yhayato1320@gmail.com>
2017-12-24 10:11:54 +0900

    minimizerの更新

--- minimizer.py
+++ minimizer.py
@@ -8,18 +8,48 @@ def f(x):
     print("x = {}, y = {}".format(x,y))
     return y
 
+def error(line, data):
+    err = np.sum((data[:, 1] - (line[0] * data[:, 0] + line[1]))**2)
+    return err
+
+def fit_line(data, error_func):
+    l = np.float32([0, np.mean(data[:, 1])])
+
+    x_ends = np.float32([-5, 5])
+    plt.plot(x_ends, l[0] * x_ends + l[1], "m--", linewidth=2.0, label="Initial guesess")
+
+    result = spo.minimize(error_func, l, args=(data, ), method="SLSQP", options={"disp": True})
+    return result.x
+
+def error_poly(C, data):
+    err = np.sum((data[:, 1] - np.polyval(C, data[:, 0])) ** 2)
+    return err
+
+def fit_poly(data, error_func, degree=3):
+    Cguess = np.poly1d(np.ones(degree+1, dtype=np.float32))
+
+    x= np.linspace(-5, 5, 21)
+    plt.plot(x, np.polyval(Cguess, x), "m--", linewidth=2.0m label="Intitial guess")
+
+    result = spo.minimize(error_func, Cguess, args=(data, ), method="SLSQP", options={"disp": True})
+    return np.poly1d(result.x)
+
 def test_run():
-    Xguess = 2.0
-    min_result = spo.minimize(f, Xguess, method="SLSQP", options = {"disp": True})
-    print("Minima found at:")
-    print("x = {}, y = {}".format(min_result.x, min_result.fun))
+    l_orig = np.float32([4, 2])
+    print("original line: C0 = {}, C1 = {}".format(l_orig[0], l_orig[1]))
+    Xorig = np.linspace(0, 10, 21)
+    Yorig = l_orig[0] * Xorig + l_orig[1]
+    plt.plot(Xorig, Yorig, "b--", linewidth=2.0, label="Orinal line")
 
-    Xplot = np.linspace(0.5, 2.5, 21)
-    Yplot = f(Xplot)
-    plt.plot(Xplot, Yplot)
-    plt.plot(min_result.x, min_result.fun, "ro")
-    plt.title("Minima of an objective function")
+    noise_sigma = 3.0
+    noise = np.random.normal(0, noise_sigma, Yorig.shape)
+    data = np.asarray([Xorig, Yorig + noise]).T
+    plt.plot(data[:, 0], data[:, 1], "go", label="Data points")
+
+    l_fit = fit_line(data, error)
+    print("Fitted line: C0 = {}, C1 = {}".format(l_fit[0], l_fit[1]))
+    plt.plot(data[:, 0], l_fit[0] * data[:, 0] + l_fit[1], "r--", linewidth=2.0)
+
     plt.show()
-
 if __name__ == "__main__":
     test_run()
