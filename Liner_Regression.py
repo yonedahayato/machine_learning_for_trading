1523522060ae2a04548e4f81b166e45d440fe09d
yoneda hayato <yhayato1320@gmail.com>
2018-03-03 17:50:12 +0900

    Liner_Regression.py

---
+++ Liner_Regression.py
@@ -0,0 +1,10 @@
+class LinerRegression:
+    def __init__():
+        pass
+
+    def tarin(self, X, y):
+        self.M, self.b = favorite_lnereg(X, y)
+
+    def query(self, X):
+        y = self.M * X + self.b
+        return y
