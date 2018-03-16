3bd0ea2b2dd71a750f83757f5dc02ab24c8f6a42
yoneda hayato <yhayato1320@gmail.com>
2018-03-16 09:20:49 +0900

    大まかな流れを作成、修正

--- simple_RL_withTensorflow/trading_env.py
+++ simple_RL_withTensorflow/trading_env.py
@@ -5,63 +5,15 @@ import pandas as pd
 import pandas_datareader.data as web
 
 class Observation:
-    def __init__(self, stock_num, data_length):
-        self.stock_num = stock_num
-        self.data_length = data_length
+    def __init__(self):
+        stock_data_list = self.load_stock_data(view_data=False)
+        self.stock_num = len(stock_data_list)
 
         self.status = {0:"not_hold", 1:"hold"]
-        self.observation_num = stock_num * len(self.status) * data_length
+        self.observation_num = self.stock_num * len(self.status) * self.data_length
         self.n = self.observation_num
 
-    def make_status(self):
-        self.hold_status = [0]*self.stock_num
-        self.status_num = 0
-        self.status_table = np.arange(self.observation_num).reshape(self.data_length, self.stock_num * len(self.status))
-
-
-    def execute(self, actions_list):
-        for Id in range(self.stock_num):
-            action = actions_list[Id]
-            hold_status = self.status[self.hold_status[Id]]
-            if action == "buy" and hold_status == "not_hold":
-                self.hold_status[Id] = 1
-            elif action == "sell" and hold_status == "hold":
-                self.hold_status[Id] = 0
-            else:
-                pass
-        return self.hold_status
-
-class Action:
-    def __init__(self, stock_num):
-        self.action_dic = {0:"buy", 1:"sell", 2:"hold"}
-        self.stock_num = stock_num
-        self.n = len(self.action_dic) ** stock_num
-
-        action_list = list(range(len(self.action_dic)))
-        action_list = list(itertools.product(action_list,repeat=stock_num))
-
-        self.action_table = np.array(action_list)
-
-    def execute(self, action):
-        actions = self.action_table[action]
-
-        actions_list = []
-        for cnt in range(self.stock):
-            action_num = actions[cnt]
-            actions_list.append(self.action_dic[action_num])
-
-        return actions_list
-
-class Trading_Env:
-    def __init__(self):
-        stock_data_list = self.stock_data_load(view_data=False)
-
-        self.observation_space = Observation(stock_num=len(stock_data_list), data_length=self.data_length)
-        self.action_space = Action(stock_num=len(stock_data_list))
-
-        self.observation_space.make_status()
-
-    def stock_data_load(self, view_data=False):
+    def load_stock_data(self, view_data=False):
         # start = datetime.datetime(2016,1,1)
         # end = datetime.date.today()
         start = "2016-01-01"
@@ -85,14 +37,69 @@ class Trading_Env:
         self.stock_data_list = stock_data_list
         return stock_data_list
 
-    def step(self, action):
-        actions_list = self.action_space.execute(action)
-        hold_status = self.observation_space.execute(action_list)
-        ####
-        self.status_num += 1
-        s1 = None
-        r = None
+    def make_status(self):
+        self.hold_status = [0]*self.stock_num
+        self.status_table = np.arange(self.observation_num).reshape(self.data_length, self.stock_num * len(self.status))
 
+    def update_status(self, action_list):
+        for Id in range(self.stock_num):
+            action = actions_list[Id]
+            hold_status = self.status[self.hold_status[Id]]
+            if action == "buy" and hold_status == "not_hold":
+                self.hold_status[Id] = 1
+            elif action == "sell" and hold_status == "hold":
+                self.hold_status[Id] = 0
+            else:
+                pass
+        return
+
+    def get_status(self):
+        pass
+
+class Action:
+    def __init__(self, stock_num):
+        self.action_dic = {0:"buy", 1:"sell", 2:"hold"}
+        self.stock_num = stock_num
+        self.n = len(self.action_dic) ** stock_num
+
+        action_list = list(range(len(self.action_dic)))
+        action_list = list(itertools.product(action_list,repeat=stock_num))
+
+        self.action_table = np.array(action_list)
+
+    def parse_action(self, action):
+        actions = self.action_table[action]
+
+        actions_list = []
+        for cnt in range(self.stock):
+            action_num = actions[cnt]
+            actions_list.append(self.action_dic[action_num])
+
+        return actions_list
+
+class Trading_Env:
+    def __init__(self):
+        self.observation_space = Observation()
+        self.observation_space.make_status()
+        stock_num = Observation.stock_num
+
+        self.action_space = Action(stock_num=stock_num)
+
+        self.step_num = 0
+
+    def calculate_reward(self):
+        pass
+
+    def step(self, action):
+        actions_list = self.action_space.parse_action(action)
+
+        self.observation_space.update_status(action_list)
+        s1 = self.get_status()
+
+        reward_info = self.observation_space.get_reward_info()
+        r = self.calculate_reward(reward_info)
+
+        self.step_num += 1
         d = None
         _ = None
 
