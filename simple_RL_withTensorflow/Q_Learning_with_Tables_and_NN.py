e991f30964558be1707aa3d6ccd26bbb7c522120
yoneda hayato <yhayato1320@gmail.com>
2018-03-20 09:00:53 +0900

    ニューラルネットワークを使用してのmain_tmpの実装、その他mainの修正

--- simple_RL_withTensorflow/Q_Learning_with_Tables_and_NN.py
+++ simple_RL_withTensorflow/Q_Learning_with_Tables_and_NN.py
@@ -1,6 +1,9 @@
 import gym
+import matplotlib.pyplot as plt
 import numpy as np
+import random
 import sys
+import tensorflow as tf
 
 from trading_env import Trading_Env
 
@@ -25,6 +28,8 @@ class LeinforceRearning():
         self.a = None
         self.s1 = None
         self.r = None
+        self.check_actions = []
+        self.check_statuses = []
 
         self.d = None
 
@@ -32,7 +37,7 @@ class LeinforceRearning():
         self.lr = 0.8
         self.y = 0.95
         self.num_episodes = 2000
-        # self.num_episodes = 10
+        self.num_episodes = 10
         self.step_num = 200
 
     def initialize_Qtable_with_zeros(self):
@@ -46,6 +51,7 @@ class LeinforceRearning():
         if Qtable:
             print("Q-table")
             print(self.Q)
+
         print("state:{}".format(self.s))
         print("action:{}".format(self.a))
         print("state1:{}".format(self.s1))
@@ -77,12 +83,23 @@ class LeinforceRearning():
         self.Q = Q.copy()
         return Q
 
-    def result(self, rList, Qtable=False):
+    def result(self, rList, Qtable=False, check=False):
         print("Score over time: " + str(sum(rList)/self.num_episodes))
         print("Step num over time: " + str(sum(self.jList)/self.num_episodes))
         if Qtable:
             print("Final Q-Table Values")
             print(self.Q)
+
+        if check:
+            print("action_space.n")
+            print(self.env.action_space.n)
+            print("actions")
+            print(self.check_actions)
+
+            print("observation_space.n")
+            print(self.env.observation_space.n)
+            print("statuses")
+            print(self.check_statuses)
 
     def train(self):
         Q = self.initialize_Qtable_with_zeros()
@@ -98,6 +115,8 @@ class LeinforceRearning():
             rAll = 0
             d = False
             j = 0
+            actions = []
+            statuses = [s]
 
             #The Q-Table learning algorithm
             while j < self.step_num: # step
@@ -107,8 +126,10 @@ class LeinforceRearning():
                 s1, r, d = self.get_new_state_reward_from_environment(action = a)
                 Q = self.update_Qtable_with_new_knowledge(s, a, r, s1)
 
-                self.status_check(episode=i+1, step=j, Qtable=False)
+                # self.status_check(episode=i+1, step=j, Qtable=False)
 
+                actions.append(a)
+                statuses.append(s1)
                 rAll += r
                 self.s = s1
                 s = s1
@@ -117,15 +138,82 @@ class LeinforceRearning():
 
             self.jList.append(j)
             self.rList.append(rAll)
-            self.result(self.rList, Qtable=False)
+            self.check_actions.append(actions)
+            self.check_statuses.append(statuses)
 
-        self.result(self.rList, Qtable=False)
+        self.result(self.rList, Qtable=False, check=True)
 
 def main():
     # LR = LeinforceRearning(game_name="FrozenLake")
     LR = LeinforceRearning(game_name="Trading")
     LR.train()
 
+def main_tmp():
+
+    env = gym.make("FrozenLake-v0")
+
+    tf.reset_default_graph()
+
+    #These lines establish the feed-forward part of the network used to choose actions
+    action_num = 16
+    inputs1 = tf.placeholder(shape=[1,action_num],dtype=tf.float32)
+    W = tf.Variable(tf.random_uniform([16,4],0,0.01))
+    Qout = tf.matmul(inputs1,W)
+    predict = tf.argmax(Qout,1)
+
+    #Below we obtain the loss by taking the sum of squares difference between the target and prediction Q values.
+    nextQ = tf.placeholder(shape=[1,4],dtype=tf.float32)
+    loss = tf.reduce_sum(tf.square(nextQ - Qout))
+    trainer = tf.train.GradientDescentOptimizer(learning_rate=0.1)
+    updateModel = trainer.minimize(loss)
+
+    # init = tf.initialize_all_variables()
+    init = tf.global_variables_initializer()
+
+    # Set learning parameters
+    y = .99
+    e = 0.1
+    num_episodes = 2000
+    #create lists to contain total rewards and steps per episode
+    jList = []
+    rList = []
+    with tf.Session() as sess:
+        sess.run(init)
+        for i in range(num_episodes):
+            #Reset environment and get first new observation
+            s = env.reset()
+            rAll = 0
+            d = False
+            j = 0
+            #The Q-Network
+            while j < 99:
+                j+=1
+                #Choose an action by greedily (with e chance of random action) from the Q-network
+                a, allQ = sess.run([predict,Qout],feed_dict={inputs1:np.identity(16)[s:s+1]})
+                if np.random.rand(1) < e:
+                    a[0] = env.action_space.sample()
+
+                #Get new state and reward from environment
+                s1,r,d,_ = env.step(a[0])
+                #Obtain the Q' values by feeding the new state through our network
+                Q1 = sess.run(Qout,feed_dict={inputs1:np.identity(16)[s1:s1+1]})
+                #Obtain maxQ' and set our target value for chosen action.
+                maxQ1 = np.max(Q1)
+                targetQ = allQ
+                targetQ[0,a[0]] = r + y*maxQ1
+                #Train our network using target and predicted Q values
+                _,W1 = sess.run([updateModel,W],feed_dict={inputs1:np.identity(16)[s:s+1],nextQ:targetQ})
+                rAll += r
+                s = s1
+                if d == True:
+                    #Reduce chance of random action as we train the model.
+                    e = 1./((i/50) + 10)
+                    break
+            jList.append(j)
+            rList.append(rAll)
+    print("Percent of succesful episodes: " + str(sum(rList)/num_episodes) + "%")
+    plt.plot(rList)
 
 if __name__ == "__main__":
     main()
+    # main_tmp()
