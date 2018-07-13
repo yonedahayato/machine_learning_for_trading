c238503441eacd9ed991027319627db507b2c336
yoneda hayato <yhayato1320@gmail.com>
2018-07-13 12:46:14 +0900

    fixed for docker

--- simple_RL_withTensorflow/docker_exe.sh
+++ simple_RL_withTensorflow/docker_exe.sh
@@ -5,10 +5,15 @@ SCRIPT_DIR=$(cd $(dirname $0); pwd)
 # install library
 # docker build -f ./dockerfile/Dockerfile_lib -t rl_lib .
 
+# contena name
+export COMPOSE_PROJECT_NAME=rl
+
 # pytest
-# env HELPER_DIR=$SCRIPT_DIR/helper docker-compose run rl pytest ./unit_test/unit_test.py -v
+# env HELPER_DIR=$SCRIPT_DIR/helper docker-compose run rl pytest ./unit_test/unit_test.py -v -m value
 
 # train and validate
 env HELPER_DIR=$SCRIPT_DIR/helper docker-compose run rl python Q_Learning_with_Tables_and_NN.py
 
+# remove docker image and process
 docker-compose down
+docker rmi rl_rl
